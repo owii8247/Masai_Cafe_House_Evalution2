35423443870d15ffcf31067a665a76a677f46abd
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2022-05-30 16:38:54 +0530

    Set timeout Set

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,21 +2,21 @@ function placeOrder(){
 
     setTimeout(function(){
         alert("Your order is accepted")
-    },0)
+    })
 
-    setInterval(function(){
+    setTimeout(function(){
         alert("Your order is being Prepared")
     },3000)
 
-    setInterval(function(){
+    setTimeout(function(){
         alert("Your order is being packed")
     },8000)
 
-    setInterval(function(){
+    setTimeout(function(){
         alert("Your order is out for delivery")
     },10000)
 
-    setInterval(function(){
+    setTimeout(function(){
         alert("Order Delivered")
     },12000)
 
